625c0bb8d4a232bc40bbefe721eff1b94108b884
Alexey <alexey.g.frolov@yandex.ru>
2023-12-02 17:12:54 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,58 @@
+import lotto_game
+
+
+
+barrels=lotto_game.get_random_unique_numbers(1,lotto_game.BARRELS_COUNT,lotto_game.BARRELS_COUNT)
+
+players = []
+
+#Создаем игроков
+
+players_count = int(input("Ввдите количество игроков: "))
+for i in range(players_count):
+    player_type = int (input("Тип игрока (человек - 1, компьютер - 2): "))
+    if player_type == 1:
+        #добавляем игрока - человека
+        player_name = input("Введите имя игрока:  ")
+        players.append(lotto_game.lotto_player(player_name,  lotto_game.player_type.man))
+    elif player_type == 2:
+        player_name = "Игрок №" + str(i)
+        players.append(lotto_game.lotto_player(player_name,  lotto_game.player_type.computer))
+
+
+
+#Играем до момента, пока не вытащены все бочки
+for i in range(lotto_game.BARRELS_COUNT):
+    barrel=barrels[i]
+    lotto_game.print_players(players)
+
+    print(f'Ход {i+1}.  Вытащили бочку с номером {barrel}:')
+    for player in players:
+        if player.ptype == lotto_game.player_type.computer:
+            if player.in_card(barrel):
+                player.add_barrel(barrel)
+                print('Игрок ' + player.player_name + f' забрал бочку {barrel}')
+
+                if player.all_filled():
+                    print('Игрок ' + player.player_name + ' ВЫИГРАЛ !!!')
+                    print(player)
+                    exit(0)
+                break
+        else:
+            player_choice = input("Продолжить (1)/Зачеркнуть (2)")
+
+            if player_choice == '1':
+                if player.in_card(barrel):
+                    print('Игрок ' + player.player_name + ' проиграл !!!')
+                    exit(0)
+            else:
+                if not player.add_barrel(barrel):
+                    print('Игрок ' + player.player_name + ' проиграл !!!')
+                    exit(0)
+                else:
+                    #проверяем, что выиграл
+                    if player.all_filled():
+                        print('Игрок ' + player.player_name + ' ВЫИГРАЛ !!!')
+                        print(player)
+                        exit(0)
+
---
+++ test_lotto_game.py
@@ -0,0 +1,52 @@
+import lotto_game
+import unittest
+
+
+class Test_lotto_game(unittest.TestCase):
+
+    def setUp(self):
+        self.player = lotto_game.lotto_player("Игрок 1", lotto_game.player_type.computer)
+
+    def tearDown(self):
+        del self.player
+
+    #Тест проверки функции добавления бочки
+    def test_add_barrel(self):
+
+        # Находим номер, котрый есть в карточке
+        b = 0
+        for i in range(lotto_game.BARRELS_COUNT):
+            if self.player.in_card(i):
+                b = i
+                break
+        self.assertTrue(self.player.add_barrel(b))
+
+        self.assertListEqual(self.player.barrels, [b])
+
+        # Находим номер, котрого нет в карточке
+
+        b = 0
+        for i in range(lotto_game.BARRELS_COUNT):
+            if not self.player.in_card(i):
+                b = i
+                break
+
+        self.assertFalse(self.player.add_barrel(b))
+
+    def test_in_card(self):
+        #Проверяем что функция опредления наличия числа в карточке работает верно.
+        for num_in_card in range(lotto_game.BARRELS_COUNT):
+
+            is_in_card = False
+            for i in range(self.player._CARD_ROWS):
+                for j in range(self.player._CARD_COLS):
+                    if self.player.card[i][j] == num_in_card:
+                        is_in_card = True
+                        break
+                if is_in_card:
+                    break
+
+            if not is_in_card:
+                self.assertFalse(self.player.in_card(num_in_card))
+            else:
+                self.assertTrue(self.player.in_card(num_in_card))
